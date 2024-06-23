e53a2d6d573bfc622278363c534e376a48142245
Paraschivoiu Alexandru <92440361+iamaxi25@users.noreply.github.com>
2024-06-23 20:54:54 +0300

    - Major updates, including searching in files, creating folder, moving files into specific folders, etc.
    Signed-off-by: iamaxi25 <alexandru.paraschivoiu@gmail.com>

--- organizator.py
+++ organizator.py
@@ -1,10 +1,12 @@
 import os
+import shutil
 
+# The directory path from where we want to clean up.
 director_path = r"C:\Users\axi\Downloads"
 
-# Dictionary used for
+# Dictionary used for storing extension types.
 file_extensions = {
-    "Documents": [".doc", ".docx", ".pdf", ".txt", ".rtf", ".odt", ".tex", ".wpd", ".md", ".wks", ".wps", ".pages"],
+    "Documents": [".doc", ".docx", ".pdf", ".txt" ".rtf", ".odt", ".tex", ".wpd", ".md", ".wks", ".wps", ".pages"],
     "Spreadsheets": [".xls", ".xlsx", ".csv", ".ods", ".xlsm", ".xlt", ".xltx", ".xltm", ".numbers"],
     "Presentations": [".ppt", ".pptx", ".odp", ".key", ".pps", ".ppsx", ".pptm"],
     "Images": [".jpg", ".jpeg", ".png", ".gif", ".bmp", ".tiff", ".svg", ".webp", ".psd", ".heic", ".ico", ".raw",
@@ -29,14 +31,58 @@ file_extensions = {
     "Backups": [".bak", ".tmp", ".old", ".bkp", ".backup", ".gho", ".iso", ".bkf", ".adi"],
     "Scripts": [".pl", ".sh", ".py", ".rb", ".js", ".php", ".asp", ".aspx", ".cgi", ".tcl", ".vbs", ".ps1"],
     "Miscellaneous": [".iso", ".bin", ".cue", ".dmg", ".toast", ".img", ".nrg", ".mds", ".mdf", ".vcd", ".ccd", ".sub"],
+    "Unknown": [],
 }
 
 
-def search_folder():
-    for root, dir, files in os.walk(director_path):  # Iterate trought each file from /Downloads folder
-        print(files)
+def search_for_specific_key(values, search_for):
+    """
+    Function that search the extension name in the dictionary and return specific key
+    :param values: file_extensions(dictionary)
+    :param search_for: extension name
+    :returns: True and the key associated with the found value
+    """
+    for key in values:
+        for value in values[key]:
+            if search_for in value:
+                return True, key
+    return False
+
+
+def create_folder(file, root):
+    """
+    Function to check if folder exist in directory, if exists pass, if not creates it.
+    :param file: File from the directory
+    :param root: Root of the specific file
+    :return:
+    """
+    extension = os.path.splitext(file)
+    if search_for_specific_key(file_extensions, extension[1]):
+        check_key = search_for_specific_key(file_extensions, extension[1])[1]
+        path = os.path.join(root, check_key)
+        if os.path.exists(path):
+            pass
+        else:
+            path = os.path.join(director_path, check_key)
+            os.mkdir(path)
+    else:
+        file_extensions['Unknown'].append(extension)
+        create_folder(file, root)
+
+
+def organize_files():
+    """
+    Function to organize the files found in a directory
+    :return:
+    """
+    for root, directory, files in os.walk(director_path):  # Iterate trought each file from /Downloads folder
         for file in files:
-            os.path.splitext(file)
+            create_folder(file, root)
+            source_path = os.path.join(root, file)
+            destination_path = os.path.join(root,
+                                            search_for_specific_key(file_extensions, os.path.splitext(file)[1])[1])
+            shutil.move(source_path, destination_path)
+        break
 
 
-search_folder()
+organize_files()
